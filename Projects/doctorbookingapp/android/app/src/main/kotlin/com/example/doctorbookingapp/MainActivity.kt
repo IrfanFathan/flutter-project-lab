3a51d30b6cd76493cd7024fed1f6e08bf9d678eb
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-07-22 21:14:57 +0530

    created project called Doctor Booking app

---
+++ Projects/doctorbookingapp/android/app/src/main/kotlin/com/example/doctorbookingapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.doctorbookingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Projects/doctorbookingapp/lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.white,
+      body: Container(
+        color: Colors.white,
+        width: 350,
+        height: 200 ,
+      ),
+    );
+  }
+}
