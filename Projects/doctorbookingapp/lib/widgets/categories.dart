63e7db10ca2170f1730f7697d453a17980f72c1d
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-07-23 22:42:34 +0530

    Add Padding and Text widget to Category for improved layout

--- Projects/doctorbookingapp/lib/widgets/categories.dart
+++ Projects/doctorbookingapp/lib/widgets/categories.dart
@@ -7,7 +7,10 @@ class Category extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [
-        
+        Padding(
+          padding: EdgeInsets.only(top: 16, left: 16),
+          child: Text("Category", style: TextStyle(fontSize: 28)),
+        ),
       ],
     );
   }
