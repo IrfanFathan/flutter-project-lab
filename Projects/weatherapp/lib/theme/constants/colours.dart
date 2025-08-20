3293d4c5e88abcfd240cc50c4e5505aefe231c4b
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-08-20 23:16:58 +0530

    Add AppColors class for theme color constants

---
+++ Projects/weatherapp/lib/theme/constants/colours.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors{
+  // App theme colors 
+  static const Color primaryColor = Color(0xFF000000);
+  static const Color secondaryColor =Color(0xFFF5F5F5);
+  static const Color accentColor = Color(0xFFFFA500);
+}
