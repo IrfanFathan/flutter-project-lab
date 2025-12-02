9b27ee5c37749e5e53a2eb3344e492243c166c53
IrfanFathan <muhammadirfanvnb@gmail.com>
2025-12-02 23:33:15 +0530

    updated flutter new state widget

---
+++ Projects/qrcode_scanner/lib/scanScreen.dart
@@ -0,0 +1,18 @@
+class MyApp extends StatefulWidget {
+    const MyApp({Key? key}) : super(key: key);
+    
+    @override
+    State<MyApp> createState() => _MyAppState();
+}
+    
+class _MyAppState extends State<MyApp> {
+        @override
+        Widget build(BuildContext context) {
+            return Scaffold(
+                appBar: AppBar(
+                    title: const Text('QR Code Scanner'),
+                ),
+                body: const ScanScreen(),
+            );
+        }
+}
